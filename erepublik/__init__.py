8690c4d3f2681740945655aac930949867ab51f0
Eriks Karls <eriks@pd.lv>
2019-07-30 11:10:35 +0300

    minor fix

--- erepublik/__init__.py
+++ erepublik/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Eriks Karls"""
 __email__ = 'eriks@72.lv'
-__version__ = '__version__ = '0.15.0'
+__version__ = '0.15.0'
 
 from erepublik import classes, utils
 from erepublik.citizen import Citizen
