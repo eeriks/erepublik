953902476f5fabd96495808987f3b3a81390f41f
Eriks Karls <eriks@pd.lv>
2019-07-30 09:32:10 +0300

    Bump version: 0.14.6.1 → 0.14.7

--- erepublik/__init__.py
+++ erepublik/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Eriks Karls"""
 __email__ = 'eriks@72.lv'
-__version__ = '0.14.6.1'
+__version__ = '__version__ = '0.14.7''
 
 from erepublik import classes, utils
 from erepublik.citizen import Citizen
--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eeriks/erepublik_script',
-    version='0.14.6',
+    version='0.14.7',
     zip_safe=False,
 )
