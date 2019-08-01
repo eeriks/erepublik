e5b7cde044a9b0a03fe6eb89a91ca17588291681
Eriks Karls <eriks@pd.lv>
2019-08-01 15:06:34 +0300

    Bump version: 0.15.0 → 0.15.1

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eeriks/erepublik_script',
-    version='0.15.0',
+    version='0.15.1',
     zip_safe=False,
 )
