8dcebdecd2cca10da1a3e878ca6ca25d345d24a0
Eriks Karls <eriks@pd.lv>
2020-02-26 18:42:25 +0200

    no message

--- erepublik/__init__.py
+++ erepublik/__init__.py
@@ -5,7 +5,7 @@
 __author__ = """Eriks Karls"""
 __email__ = 'eriks@72.lv'
 __version__ = '0.20.0'
-__commit_id__ = "04bb0be"
+__commit_id__ = "77bcfb3"
 
 from erepublik import classes, utils
 from erepublik.citizen import Citizen
