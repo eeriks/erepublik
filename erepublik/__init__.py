6abfc98fbd30b401de00093ae508a02047c48ed3
Eriks K <eriks@72.lv>
2020-06-18 10:13:55 +0300

    Test requirements

--- erepublik/__init__.py
+++ erepublik/__init__.py
@@ -5,7 +5,7 @@
 __author__ = """Eriks Karls"""
 __email__ = 'eriks@72.lv'
 __version__ = '0.20.1'
-__commit_id__ = "7cf6cf0"
+__commit_id__ = "66f0e64"
 
 from erepublik import classes, utils
 from erepublik.citizen import Citizen
