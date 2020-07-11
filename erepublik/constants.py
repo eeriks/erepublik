c9710733e2efd3d29eb1333a3ffefb038761ea7d
Eriks Karls <eriks@karls.lv>
2020-07-11 09:37:22 +0300

    Typo

--- erepublik/constants.py
+++ erepublik/constants.py
@@ -47,7 +47,7 @@ class Country:
 
 class Industries:
     __by_name = {'food': 1, 'weapons': 2, 'house': 4, 'aircraft': 23,
-                 'foodRaw': 7, 'weaponsRaw': 12, 'houseRaw': 18, 'aircraftRaw': 24,
+                 'foodRaw': 7, 'weaponRaw': 12, 'weaponsRaw': 12, 'houseRaw': 18, 'aircraftRaw': 24,
                  'frm': 7, 'wrm': 12, 'hrm': 18, 'arm': 24,
                  'frm q1': 7, 'frm q2': 8, 'frm q3': 9, 'frm q4': 10, 'frm q5': 11,
                  'wrm q1': 12, 'wrm q2': 13, 'wrm q3': 14, 'wrm q4': 15, 'wrm q5': 16,
