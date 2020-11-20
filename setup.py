eb740c60c73b3122cd0616d93bd01af34683a4a3
Eriks K <eriks@72.lv>
2020-11-20 18:54:20 +0200

    Bump version: 0.22.3.2 → 0.22.3.3

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eeriks/erepublik/',
-    version='0.22.3.2',
+    version='0.22.3.3',
     zip_safe=False,
 )
