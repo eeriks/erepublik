c874247335492b1a8d67bdf4ac564856bb9b7bf1
Eriks K <eriks@72.lv>
2020-11-26 18:32:46 +0200

    Bump version: 0.22.3.3 → 0.23.0

--- erepublik/__init__.py
+++ erepublik/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Eriks Karls"""
 __email__ = 'eriks@72.lv'
-__version__ = '0.22.3.3'
+__version__ = '0.23.0'
 
 from erepublik import classes, utils, constants
 from erepublik.citizen import Citizen
