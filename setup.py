0119ebe44bd445c4b3be17baa0a11dd015c81ecb
KEriks <git@72.lv>
2022-01-06 10:11:50 +0200

    Bump version: 0.28.0.4 → 0.28.1

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/eeriks/erepublik/",
-    version="0.28.0.4",
+    version="0.28.1",
     zip_safe=False,
 )
